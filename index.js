90f6543924a0fb5e4d940eb41f8bedcd568d504e
Sukanta Mahunta <sukanta.sm01@gmail.com>
2024-11-14 22:50:06 +0530

    .env configured

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 const routes = require("./routes/Routes");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
+
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
