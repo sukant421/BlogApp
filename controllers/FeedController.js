c7afa7d75074794958b19b6cf606e9f850b34a18
Sukanta Mahunta <sukanta.sm01@gmail.com>
2025-01-05 13:53:36 +0530

    .env modification

--- controllers/FeedController.js
+++ controllers/FeedController.js
@@ -2,5 +2,8 @@ const dbJson = require("../DummyDb.json");
 
 exports.getFeed = (req, res) => {
   const feedData = dbJson.blogs;
-  res.send({ data: process.env.JWT_SECRET_KEY || "No Secret Key" });
+  res.send({
+    data1: process.env.JWT_SECRET_KEY || "No Secret Key",
+    data2: process.env._JWT_SECRET_KEY || "No Secret Key2",
+  });
 };
