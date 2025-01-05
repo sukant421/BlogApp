f6c9d15b1e2c37e50645adcdcdba9e56f1af643a
Sukanta Mahunta <sukanta.sm01@gmail.com>
2025-01-05 14:15:54 +0530

    leisure code removed

--- controllers/FeedController.js
+++ controllers/FeedController.js
@@ -4,6 +4,5 @@ exports.getFeed = (req, res) => {
   const feedData = dbJson.blogs;
   res.send({
     data1: process.env.JWT_SECRET_KEY || "No Secret Key",
-    data2: process.env._JWT_SECRET_KEY || "No Secret Key2",
   });
 };
